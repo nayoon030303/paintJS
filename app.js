c287c707a49d9c4befd509b670fcf7d76254ee99
nayoon030303 <nayoon030303@gmail.com>
2021-05-14 23:21:56 +0900

    Brush Size

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange")
+const mode = document.getElementById("jsMode");
 
 //pixel을 다루는 윈도우의 크기
 canvas.width = 700;
@@ -10,6 +12,7 @@ ctx.strokeStyle = "#2c2c2c"; //색성
 ctx.lineWidth = 2.5; //선 두께
 
 let painting = false;
+let fiiling = false;
 
 function startPainting(){
     painting = true;
@@ -43,12 +46,38 @@ function hadleColorClick(event){
     ctx.strokeStyle=color;
 }
 
+function handleRangeChange(event){
+    const range =event.target.value;
+    ctx.lineWidth = range;
+   
+}
+
+function handleModeClick(event){
+    if(fiiling){
+        fiiling = false;
+        mode.innerText = "Fill";
+    }else{
+        fiiling = true;
+        mode.innerText = "Paint";
+    }
+}
+
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
 }
+
+
 Array.from(colors).forEach(color=>{
     color.addEventListener("click",hadleColorClick);
 });
+
+if(range){
+    range.addEventListener("input",handleRangeChange);
+}
+
+if(mode){
+    mode.addEventListener("click",handleModeClick);
+}
