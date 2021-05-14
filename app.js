9a5b43053a0641161e39c7a172038d70a88a8a9f
nayoon030303 <nayoon030303@gmail.com>
2021-05-14 23:45:26 +0900

    Filling Mode

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange")
 const mode = document.getElementById("jsMode");
-
+const INITAL_COLOR = "#2c2c2c";
 //pixel을 다루는 윈도우의 크기
 canvas.width = 700;
 canvas.height = 700;
 
-ctx.strokeStyle = "#2c2c2c"; //색성
+ctx.strokeStyle = INITAL_COLOR; //색성
+ctx.fillStyle = INITAL_COLOR;
 ctx.lineWidth = 2.5; //선 두께
+
 
 let painting = false;
 let fiiling = false;
@@ -42,8 +44,8 @@ function onMouseDown(event){
 
 function hadleColorClick(event){
     const color = event.target.style.backgroundColor;
-    console.log(color);
-    ctx.strokeStyle=color;
+    ctx.fillStyle = color;
+    ctx.strokeStyle = color;
 }
 
 function handleRangeChange(event){
@@ -59,14 +61,27 @@ function handleModeClick(event){
     }else{
         fiiling = true;
         mode.innerText = "Paint";
+        
+
     }
 }
+
+function handleCanvasClick(){
+    
+    if(fiiling){
+        console.log(ctx.fillSyle);
+        //ctx.fillRect(0,0,canvas.width,canvas.height);
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+    }
+}
+    
 
 if(canvas){
     canvas.addEventListener("mousemove",onMouseMove);
     canvas.addEventListener("mousedown",startPainting);
     canvas.addEventListener("mouseup",stopPainting);
     canvas.addEventListener("mouseleave",stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
 }
 
 
